91138c70672e30d67fbad8a2b49ad7024ae781b4
adrianovolter <adriano.j.volter@hotmail.com>
2023-11-08 20:47:33 -0300

    feat: criado função gera string com 6 digitos

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Encurtador URL' });
 });
 
+
+function generateRandomString() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < 6; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
+
 router.post('/new', function(req, res, next) {
   const url = req.body.url;
   console.log(url);
-    res.send(url);
+  const short = generateRandomString();
+    res.send('http://localhost:3000/'+short);
 }
 );
 
