f052ec8d03f2c906581a18f1f4684e65cffb6edc
adrianovolter <adriano.j.volter@hotmail.com>
2023-11-08 20:29:46 -0300

    feat: criado metodo post

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Encurtador URL' });
 });
 
+router.post('/new', function(req, res, next) {
+  const url = req.body.url;
+  console.log(url);
+    res.send(url);
+}
+);
+
 module.exports = router;
