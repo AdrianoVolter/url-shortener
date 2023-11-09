9011a5192136fcc6b934a25f73d03f0e163ba05f
adrianovolter <adriano.j.volter@hotmail.com>
2023-11-09 19:58:00 -0300

    feat: adicionanado numero de acessos

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,58 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Link = require('../models/links');
+const Link = require("../models/links");
 
 
-router.get('/:short', async (req, res, next)=> {
+router.get("/:short/stats", async (req, res, next) => {
   const short = req.params.short;
   const result = await Link.findOne({
     where: {
-      short
-    }
+      short,
+    },
   });
-  if(result){
-    res.redirect(result.dataValues.url);
-  }else{
-    res.status(404).send('Not Found');
-  }
+ if(!result)return res.status(404).send("Not Found");
+  res.render("stats", result.dataValues);
 }
 );
 
+router.get("/:short", async (req, res, next) => {
+  const short = req.params.short;
+  const result = await Link.findOne({
+    where: {
+      short,
+    },
+  });
+  if (!result) return res.status(404).send("Not Found");
 
+  result.hits ++;
+  await result.save();
+  res.redirect(result.url);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Encurtador URL' });
 });
 
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Encurtador URL" });
+});
 
 function generateRandomString() {
   var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   for (var i = 0; i < 6; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   return text;
 }
 
-router.post('/new', async (req, res, next)=> {
+router.post("/new", async (req, res, next) => {
   const url = req.body.url;
   const short = generateRandomString();
   const result = await Link.create({
     url,
-    short
- });
-  
-  res.render('stats', result.dataValues);
-  
+    short,
+  });
+
+  res.render("stats", result.dataValues);
 });
 
 module.exports = router;
