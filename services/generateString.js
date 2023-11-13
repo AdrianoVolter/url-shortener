18b97ee12ccadf762e8fbdb487e6d4e26425f4fb
adrianovolter <adriano.j.volter@hotmail.com>
2023-11-13 09:53:37 -0300

    criando un services para função gerar string

---
+++ services/generateString.js
@@ -0,0 +1,11 @@
+
+function generateRandomString () {
+    var text = "";
+    var possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 6; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+
+module.exports = generateRandomString;
