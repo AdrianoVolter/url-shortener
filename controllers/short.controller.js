442d1c81f46a1bf9e869b012bd3b0c336a9b3dac
adrianovolter <adriano.j.volter@hotmail.com>
2023-11-13 09:52:42 -0300

    criando as funções dos endpoints

--- controllers/short.controller.js
+++ controllers/short.controller.js
@@ -1,4 +1,5 @@
 const  Link  = require("../models/links");
+const generateRandomString = require("../services/generateString");
 
 module.exports= {
     async stats (req, res) {
@@ -14,5 +15,38 @@ module.exports= {
         if (!result) return res.status(404).send("Not Found");
 
         res.render("stats", result.dataValues);
+    },
+
+    async getShort (req, res) {
+        const short = req.params.short;
+        
+        const result = await Link.findOne({
+            where: {
+                short
+            }
+        });
+
+        if (!result) return res.status(404).send("Not Found");
+
+        result.hits++;
+        await result.save();
+
+        res.redirect(result.url);
+    },
+    
+    async new (req, res) {
+        const url = req.body.url;
+        const short = generateRandomString();
+        const result = await Link.create({
+            url,
+            short,
+        });
+
+        res.render("stats", result.dataValues);
+    },
+
+    async homePage (req, res) {
+        res.render("index", { title: "Encurtador URL" });
     }
+       
 }
